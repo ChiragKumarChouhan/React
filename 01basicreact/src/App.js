895e42ceaa8eb10f7ae5ec3310de860e4304d674
ChiragKumarChouhan <chiragneeraj2@gmail.com>
2026-06-26 16:50:35 +0530

    react 2

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+
+
+
+// eslint-disable-next-line no-unused-vars
+function App() {
+  return (
+  <h1>
+this is a react learning journey | chirag kumar | today i learn how to create project in react using vite and how packates work also we make a repo on github and push that files
+  </h1>
+
+  );
+}
+export default App;
---
+++ vite/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+   <h1>
+    react
+   </h1>
+  )
+}
+
+export default App
